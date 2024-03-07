b056a221e344dc9bb191362eeb0252f62b96c8c9
unknown <rajaosama.me@gmail.com>
2024-03-07 16:41:42 +0400

    integration

--- src/components/mui/styles.ts
+++ src/components/mui/styles.ts
@@ -1,5 +1,2 @@
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-export {
-    ThemeProvider, 
-    createTheme
-}
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+export { ThemeProvider, createTheme }
